1bb63486ce7c775f185e5c02080f9e5268db1fba
Uros <uavramovic@digicompanion.com>
2013-12-27 23:37:47 +0100

    brave little soldier

--- src/com/example/dailywin/Edit.java
+++ src/com/example/dailywin/Edit.java
@@ -1,6 +1,8 @@
 package com.example.dailywin;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,11 +20,13 @@ public class Edit extends Activity {
 
     private Edit self;
     private Button saveButton;
+    private Button deleteButton;
     private RadioGroup radioGroup;
     private SeekBar seekBar;
     private EditText name;
     private MyDB db;
     private String activity_id = "";
+    private String activity_f = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,18 +34,19 @@ public class Edit extends Activity {
         if(getIntent() != null && getIntent().getExtras() != null){
             activity_id = getIntent().getStringExtra("activity_id");
             activity_name = getIntent().getStringExtra("activity_name");
-            activity_freq = getIntent().getStringExtra("activity_freq");
+            activity_f = getIntent().getStringExtra("activity_freq");
             activity_importance = getIntent().getStringExtra("activity_importance");
         }
 
         super.onCreate(savedInstanceState);
         self = this;
-        int resourceId = getResources().getIdentifier(activity_freq, "id", getPackageName());
+        int resourceId = getResources().getIdentifier(activity_f, "id", getPackageName());
 
         setContentView(R.layout.edit);
         name = (EditText) findViewById(R.id.editText);
         name.setText(activity_name);
         saveButton = (Button) findViewById(R.id.saveButton);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
         radioGroup = (RadioGroup) findViewById(R.id.radioFrequency);
         radioGroup.getCheckedRadioButtonId();
         radioGroup.check(resourceId);
@@ -61,6 +66,27 @@ public class Edit extends Activity {
                 passCategory.putExtra("freq_filter", freq);
                 startActivity(passCategory);
                 // finish();
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(self).setTitle("Notice").setMessage("Are you serial?")
+                        .setNegativeButton("Nja", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setPositiveButton("M'kay", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                db.archiveEvent(Integer.parseInt(activity_id));
+
+                                Intent passCategory = new Intent(self, MyActivity.class);
+                                passCategory.putExtra("freq_filter", activity_f);
+                                startActivity(passCategory);
+                            }
+                        }).show();
             }
         });
 
