c3f07275352edfacf67aeb635c68acb582796ccf
z0z0 <vasiljevic.zorana@gmail.com>
2014-02-08 18:18:24 +0100

    plain messages insert

--- src/com/example/dailywin/db/ElementHelper.java
+++ src/com/example/dailywin/db/ElementHelper.java
@@ -20,8 +20,45 @@ public class ElementHelper extends SQLiteOpenHelper {
     private static final String DATABASE_CREATE_TABLE3 = "create table Badge ( _id integer primary key, name text not null, trigger integer not null, badge_type_id integer not null, icon_url text not null, text text not null );";
     private static final String DATABASE_CREATE_TABLE4 = "create table BadgeType ( _id integer primary key, name text not null);";
     private static final String DATABASE_CREATE_TABLE5 = "create table UserBadge ( _id integer primary key, badge_id integer not null, timestamp DATETIME not null);";
+    private static final String DATABASE_CREATE_TABLE6 = "create table PlainMessage ( _id integer primary key, text text not null;";
 
 
+    private static final String TABLE6_INITIAL_INSERT = "INSERT INTO plain_message (text) VALUES " +
+            " ('You nailed it!'), " +
+            " ('Rocked it today!'), " +
+            " ('Wow. Impressive.'), " +
+            " ('Little things matter!'), " +
+            " ('Nicely done!'), " +
+            " ('Score!'), " +
+            " ('Holla!'), " +
+            " ('Woop dee do!'), " +
+            " ('sharp as a witch\"s boob / beek'), " +
+            " ('out of the park! Homerun.'), " +
+            " ('Shazaaaam'), " +
+            " ('Fancy shmancy'), " +
+            " ('You nailed it!'), " +
+            " ('You put lesser beings to shame'), " +
+            " ('Pat yourself on the back for this one.'), " +
+            " ('There are no words to adequately praise this accomplishment.'), " +
+            " ('Giddy Up!'), " +
+            " ('Hells to the yes!'), " +
+            " ('True story.'), " +
+            " ('Challenge accepted.'), " +
+            " ('Freeeeedom!'), " +
+            " ('Here\\'s looking at you, kid.'), " +
+            " ('May the Force be with you.'), " +
+            " ('You talking to me?'), " +
+            " ('Made it, Ma! Top of the world!'), " +
+            " ('I am big! It\\'s the pictures that got small.'), " +
+            " ('Round up the usual suspects.'), " +
+            " ('You\\'re back!'), " +
+            " ('It's alive! It's alive!'), " +
+         //   " ('As God is my witness, I\\'ll never be hungry again.'), " +
+            " ('You brave little soldier! You\\'re too precious for this world!'), " +
+            " ('What are the odds?'), " +
+            " ('Live long and prosper.'), " +
+            " ('Aces high!'), " +
+            " ('You mother would be so proud!');";
 
     public ElementHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -33,6 +70,7 @@ public class ElementHelper extends SQLiteOpenHelper {
         db.execSQL(DATABASE_CREATE_TABLE3);
         db.execSQL(DATABASE_CREATE_TABLE4);
         db.execSQL(DATABASE_CREATE_TABLE5);
+        db.execSQL(DATABASE_CREATE_TABLE6);
     }
 
     @Override
